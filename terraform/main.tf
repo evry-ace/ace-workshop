d032ae457e69dd801b803d7d86cf09017c4cdfa3
Hans Kristian Flaatten <hans.flaatten@evry.com>
2019-08-21 22:24:49 +0200

    Run Terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,32 +3,32 @@ data "azurerm_resource_group" "rg-ws" {
 }
 
 module "acr" {
-  source        = "github.com/evry-ace/tf-azure-acr"
-  registry_name = "acews${var.name}"
+  source              = "github.com/evry-ace/tf-azure-acr"
+  registry_name       = "acews${var.name}"
   resource_group_name = data.azurerm_resource_group.rg-ws.name
-  sku = "Standard"
+  sku                 = "Standard"
 }
 
 resource "azurerm_log_analytics_workspace" "la-ws" {
-  name = "la-ws-${var.name}"
+  name                = "la-ws-${var.name}"
   resource_group_name = data.azurerm_resource_group.rg-ws.name
-  location = data.azurerm_resource_group.rg-ws.location
-  sku ="PerGB2018"
+  location            = data.azurerm_resource_group.rg-ws.location
+  sku                 = "PerGB2018"
 }
 
 module "aks" {
-  source = "github.com/evry-ace/tf-azure-aks"
-  cluster_name   = "aks-ws-${var.name}"
+  source              = "github.com/evry-ace/tf-azure-aks"
+  cluster_name        = "aks-ws-${var.name}"
   resource_group_name = data.azurerm_resource_group.rg-ws.name
 
-  rbac_server_app_id = var.aks_rbac_server_app_id
+  rbac_server_app_id     = var.aks_rbac_server_app_id
   rbac_server_app_secret = var.aks_rbac_server_app_secret
-  rbac_client_app_id = var.aks_rbac_client_app_id
+  rbac_client_app_id     = var.aks_rbac_client_app_id
 
   dns_prefix = "ace-ws-${var.name}"
 
-  client_id = var.client_id
-  client_secret = var.client_secret
+  client_id      = var.client_id
+  client_secret  = var.client_secret
   ssh_public_key = var.ssh_public_key
 
   oms_agent_enable = false
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,17 +7,17 @@ variable "location" {
 variable "resource_group" {
 }
 
-variable "client_id" {  
+variable "client_id" {
 }
 
-variable "client_secret" {  
+variable "client_secret" {
 }
 
-variable "tenant_id" {  
+variable "tenant_id" {
 }
 
 variable "subscription_id" {
-  
+
 }
 
 variable "aks_rbac_client_app_id" {
@@ -28,7 +28,7 @@ variable "aks_rbac_server_app_id" {
 
 variable "aks_rbac_server_app_secret" {
 }
- 
+
 variable "ssh_public_key" {
   default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDTMK+HzpmWf50Y7kbKR4YAWfLujeY7BGxBpVBd9eDA7SPT7bRYzlu2wn4kJVXPkrlNKD8Qd8CFLP6ABlXcBIopqhMrQ4FBNQBe4uVEQhfpFwcD54KDpXPROlK0ydBN527arvEOtdF+bJrxx5FWr2g4kCK+lLf0X5d2TZ1T/gd5ejpZbwzDxvJi5mJAj2kLQW2FUTWMwXoxrjJM/B6STxz7qRdlL5ljQIQ+klF0R6lpbh/2o2PMpe6bRrxt6lA3oNwBJFV0ZXJijAgdVmTL62v3ZYgkBrbFTe3sRFPI6i2s2LZzrSJB0gInRC0aLqxBZhWTBjH/Itml1LzYDQbNoGCT ekarlso@ekarlso-p1"
 }
