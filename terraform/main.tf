110c829b4c97ddc09cc2fd0f7c9f6142da918fe1
Hans Kristian Flaatten <hans.flaatten@evry.com>
2019-08-21 22:35:35 +0200

    Update variables according to Jenkins credentials

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,25 @@
-data "azurerm_resource_group" "rg-ws" {
-  name = var.resource_group
+data "azurerm_resource_group" "ws" {
+  name = var.azure_resource_group
 }
 
 module "acr" {
   source              = "github.com/evry-ace/tf-azure-acr"
   registry_name       = "acews${var.name}"
-  resource_group_name = data.azurerm_resource_group.rg-ws.name
+  resource_group_name = data.azurerm_resource_group.ws.name
   sku                 = "Standard"
 }
 
-resource "azurerm_log_analytics_workspace" "la-ws" {
-  name                = "la-ws-${var.name}"
-  resource_group_name = data.azurerm_resource_group.rg-ws.name
-  location            = data.azurerm_resource_group.rg-ws.location
+resource "azurerm_log_analytics_workspace" "ws" {
+  name                = "ws-${var.name}"
+  resource_group_name = data.azurerm_resource_group.ws.name
+  location            = data.azurerm_resource_group.ws.location
   sku                 = "PerGB2018"
 }
 
 module "aks" {
   source              = "github.com/evry-ace/tf-azure-aks"
   cluster_name        = "aks-ws-${var.name}"
-  resource_group_name = data.azurerm_resource_group.rg-ws.name
+  resource_group_name = data.azurerm_resource_group.ws.name
 
   rbac_server_app_id     = var.aks_rbac_server_app_id
   rbac_server_app_secret = var.aks_rbac_server_app_secret
@@ -27,12 +27,12 @@ module "aks" {
 
   dns_prefix = "ace-ws-${var.name}"
 
-  client_id      = var.client_id
-  client_secret  = var.client_secret
-  ssh_public_key = var.ssh_public_key
+  client_id      = var.azure_client_id
+  client_secret  = var.azure_client_secret
+  ssh_public_key = var.aks_ssh_public_key
 
   oms_agent_enable = false
-  oms_workspace_id = azurerm_log_analytics_workspace.la-ws.id
+  oms_workspace_id = azurerm_log_analytics_workspace.ws.id
 
   k8s_version = "1.14.6"
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-  subscription_id            = "${var.subscription_id}"
-  client_id                  = "${var.client_id}"
-  client_secret              = "${var.client_secret}"
-  tenant_id                  = "${var.tenant_id}"
+  subscription_id            = "${var.azure_subscription_id}"
+  client_id                  = "${var.azure_client_id}"
+  client_secret              = "${var.azure_client_secret}"
+  tenant_id                  = "${var.azure_tenant_id}"
   version                    = "v1.32.1"
   skip_provider_registration = true
 }
