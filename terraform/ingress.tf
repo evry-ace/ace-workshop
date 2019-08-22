7c7538bc90f664b478018b459b63960f9d0f30c9
Hans Kristian Flaatten <hans.flaatten@evry.com>
2019-08-22 14:29:39 +0200

    Install Traefik Helm Chart for Ingress

---
+++ terraform/ingress.tf
@@ -0,0 +1,12 @@
+data "helm_repository" "stable" {
+    name = "stable"
+    url  = "https://kubernetes-charts.storage.googleapis.com"
+}
+
+resource "helm_release" "traefik" {
+  name       = "traefik"
+  namespace  = "kube-system"
+  repository = "${data.helm_repository.stable.metadata.0.name}"
+  chart      = "traefik"
+  version    = "1.76.1"
+}
