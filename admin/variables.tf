20192ea06150048d61f54046fb0bd2583b94e906
Hans Kristian Flaatten <hans.flaatten@evry.com>
2019-08-26 10:10:15 +0200

    Add all the users

--- admin/variables.tf
+++ admin/variables.tf
@@ -21,5 +21,47 @@ variable "users" {
   default = [{
     id  = "hans"
     oid = "864c7087-d2a8-4ca8-84e0-f8c272d27311"
+  }, {
+    id  = "anders"
+    oid = "e5bf60dc-749e-48c6-aa9e-a656ca47abfa"
+  }, {
+    id  = "daniel"
+    oid = "6bd0d05d-f37e-49b9-a2cd-07e437dc5efc"
+  }, {
+    id  = "igor"
+    oid = "85843441-5a38-4055-b67e-b1c97f4b94f9"
+  }, {
+    id  = "jimmy"
+    oid = "d11ad384-1221-41e7-81c9-46c51d5095e3"
+  }, {
+    id  = "nicklas"
+    oid = "7efd6afb-0ead-4865-8dda-2bb7f88902d6"
+  }, {
+    id  = "oystein"
+    oid = "bb19d995-b8c0-4a6d-bd4c-32f6a65ef4de"
+  }, {
+    id  = "par"
+    oid = "3ff40176-ddd9-4652-af2f-91bdc9c2834f"
+  }, {
+    id  = "rickard"
+    oid = "dff5661f-9880-49ad-af8a-b05529be8498"
+  }, {
+    id  = "roger"
+    oid = "ad0fffcb-4abc-4765-81e9-928418936296"
+  }, {
+    id  = "ronny"
+    oid = "8992e341-b34a-416c-9646-6f86bf7dce2d"
+  }, {
+    id  = "steinar"
+    oid = "b1f654ca-b374-4fa8-b8ea-a4836cdaf120"
+  }, {
+    id  = "viorica"
+    oid = "4d2d1933-9ac4-46ae-b4a6-d6d2c00ea4bb"
+  }, {
+    id  = "olga"
+    oid = "06c4e235-d9cb-4670-b85a-2ee218a37674"
+  }, {
+    id  = "shamrez"
+    oid = "a95c02db-2eb8-43a9-939e-7d2d5a5824ed"
   }]
 }
